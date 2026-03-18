78876b36b4e6e911f81a293cf201b436e181650b
ReStack3 <246109284@qq.com>
2026-03-18 14:51:41 +0800

    更新 .gitignore，忽略 IDE 配置和测试文件夹

--- test/Draft/erccommon/eventConfig.go
+++
@@ -1,5 +0,0 @@
-package erccommon
-
-type EventPaserConfig map[MethodName]EventParserFunc
-
-type EventParserFunc func(data []byte) (TokenEvent, error)
--- test/Draft/getpair.go
+++
@@ -1,7 +0,0 @@
-//package main
-//
-//import arbitrage "github.com/Crypto-ETHSentinel/internal/Arbitrageur/tools"
-//
-//func main() {
-//	print(arbitrage.Get_Uniswap_AllPairs())
-//}
